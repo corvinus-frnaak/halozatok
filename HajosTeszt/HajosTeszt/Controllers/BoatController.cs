d5230be24e55ea7d03e11017af1509947a546a1d
Csaba Kristóf Johanyák <csaba.johanyak@stud.uni-corvinus.hu>
2021-05-02 23:36:33 +0200

    10.

--- HajosTeszt/HajosTeszt/Controllers/BoatController.cs
+++ HajosTeszt/HajosTeszt/Controllers/BoatController.cs
@@ -32,5 +32,14 @@ namespace HajosTeszt.Controllers
 
             return new JsonResult(kérdés);
         }
+        [HttpGet]
+        [Route("questions/count")]
+        public int M4() 
+        {
+            HajostesztContext context = new HajostesztContext();
+            int kérdésekSzáma = context.Questions.Count();
+
+            return kérdésekSzáma;
+        }
     }
 }
